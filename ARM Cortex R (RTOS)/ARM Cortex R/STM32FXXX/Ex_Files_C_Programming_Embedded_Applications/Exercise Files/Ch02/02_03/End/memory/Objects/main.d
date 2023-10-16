3de47b0ba29e6a076820d8c6b0378d04322c4cdb
Asad Rehman <asadrehman65@gmail.com>
2023-10-16 15:59:09 +0200

    New directory structure and organization.

---
+++ ARM Cortex R (RTOS)/ARM Cortex R/STM32FXXX/Ex_Files_C_Programming_Embedded_Applications/Exercise Files/Ch02/02_03/End/memory/Objects/main.d
@@ -0,0 +1,1 @@
+./objects/main.o: main.c C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h
---
+++ ARM Cortex R (RTOS)/ARM Cortex R/STM32FXXX/Ex_Files_C_Programming_Embedded_Applications/Exercise Files/Ch06/06_04/fpu/edited/Objects/main.d
@@ -0,0 +1,9 @@
+./objects/main.o: main.c C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\math.h \
+  C:\Keil_v5\ARM\INC\ST\STM32F3xx\stm32f3xx.h \
+  C:\Keil_v5\ARM\INC\ST\STM32F3xx\stm32f303x8.h \
+  C:\Keil_v5\ARM\CMSIS\Include\core_cm4.h \
+  C:\Keil_v5\ARM\CMSIS\Include\core_cmInstr.h \
+  C:\Keil_v5\ARM\CMSIS\Include\core_cmFunc.h \
+  C:\Keil_v5\ARM\CMSIS\Include\core_cm4_simd.h \
+  C:\Keil_v5\ARM\INC\ST\STM32F3xx\system_stm32f3xx.h
